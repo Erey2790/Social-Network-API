86fe33031914b5d165bbf523580d5bd59261a21e
ElmerPC <erey2790@gmail.com>
2022-04-04 01:05:14 -0500

    finished user.js

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const { Schema, model } = require('mongoose');
 
-const UserSchema = new Schema({
+const UserSchema = new Schema(
+ {
     username: {
         type: String,
         unique: true,
@@ -15,13 +16,30 @@ const UserSchema = new Schema({
     },
     thoughts: [
         {
-            type: '',
+            type: Schema.Types.ObjectId,
             ref: 'Thought'
         }
     ],
-    friends: {
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+ },
+ {
+     toJSON: {
+         virtuals: true,
+         getters: true
+     },
+     id: false
+ }
+);
 
-    }
+UserSchema.virtual('friendCount').get(function() {
+    return this.friends.length;
 })
 
-const User = model('User', UserSchema)
+const User = model('User', UserSchema);
+
+module.exports = User;
