f524b0e500f1b0c51a5ab8c980fb438c8085a7ba
ElmerPC <erey2790@gmail.com>
2022-04-04 02:02:28 -0500

    fixed thoughts.js

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,31 @@
 const { Schema, model, Types } = require('mongoose');
 
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: new Types.ObjectId()
+
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280
+        },
+        username: {
+            Type: String,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => dateFormat(createdAtVal)
+
+        }
+    }
+)
+
 const ThoughtSchema = new Schema (
     {
         thoughtText: {
@@ -29,7 +55,7 @@ const ThoughtSchema = new Schema (
 );
 
 ThoughtSchema.virtual('reactionCount').get(function() {
-    return this.reactions.length;
+    return this.reaction.length;
 })
 
 
