6cf4257f6a46a22d19cefa74e32b8fce5aac4381
ElmerPC <erey2790@gmail.com>
2022-04-06 22:43:43 -0500

    added delete and update thought

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -68,26 +68,25 @@ updateThought({ params, body }, res) {
 
   // delete thought
   deleteThought({ params }, res) {
-    Thought.findOneAndDelete({ _id: params.thoughtId })
-      .then(deletedThought => {
-        if (!deletedThought) {
-          return res.status(404).json({ message: 'No thought with this id!' });
+    // delete the thought
+    Thought.findOneAndDelete({ _id: params.id })
+    .then(dbThoughtData => {
+        if (!dbThoughtData) {
+            res.status(404).json({ message: 'No thought found with this id'});
+            return;
         }
-        return User.findOneAndUpdate(
-          { _id: params.userId },
-          { $pull: { thought: params.thoughtId } },
-          { new: true }
-        );
-      })
-      .then(dbUserData => {
-        if (!dbUserData) {
-          res.status(404).json({ message: 'No User found with this id!' });
-          return;
-        }
-        res.json(dbUserData);
-      })
-      .catch(err => res.json(err));
-  },
+        // delete the reference to deleted thought in user's thought array
+        User.findOneAndUpdate(
+            { username: dbThoughtData.username },
+            { $pull: { thoughts: params.id } }
+        )
+        .then(() => {
+            res.json({message: 'Successfully deleted the thought'});
+        })
+        .catch(err => res.status(500).json(err));
+    })
+    .catch(err => res.status(500).json(err));
+},
 };
 
 module.exports = thoughtController;
